212bdd87120b40c0b2b942593257bd16ea19acd5
Jeongmmin <seo88kr@hanmail.net>
2022-08-06 23:35:51 +0900

    Add Now Playing Page

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-const screenHeader = document.querySelector('.screen-header__icons');
-const playBtn = screenHeader.querySelector('.fa-music');
+const progressbarField = document.querySelector('.progress-bar__total-play-part');
+const played = progressbarField.querySelector('.progress-bar__played-part');
+const playnow = progressbarField.querySelector('.progress-bar__play-now-part');
+
+const controllerField = document.querySelector('.music-controller-box');
+const playBtn = controllerField.querySelector('.controller__playBtn i');
 const music = new Audio('music.mp3');
 
 let play = false;
 
+
 playBtn.addEventListener('click', () => {
   play = !play;
   if (play === false) {
-    pauseSound(music);
-    playBtn.classList.remove('play-music-now');
+    stopSound(music);
   } else {
     playSound(music);
-    playBtn.classList.add('play-music-now');
+    
   }
 });
 
 function playSound(sound) {
   sound.play();
   sound.currentTime = 0;
+  playBtn.classList.remove('fa-circle-play');
+  playBtn.classList.add('fa-circle-stop');
+  played.classList.add('played-animation');
+    playnow.classList.add('play-now-animation');
 }
 
-function pauseSound(sound) {
+function stopSound(sound) {
   sound.pause();
+  playBtn.classList.remove('fa-circle-stop');
+  playBtn.classList.add('fa-circle-play');
+  played.classList.remove('played-animation');
+    playnow.classList.remove('play-now-animation');
 }
